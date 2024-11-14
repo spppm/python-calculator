9ff3a46f274b68790cbc23549ac57b71d6ad804c
Poomrapee Moungnoi <poomrapeesun2@gmail.com>
2024-11-14 14:08:21 +0000

    Edit function & Added the test case

--- calculator.py
+++ calculator.py
@@ -3,25 +3,71 @@ class Calculator:
         return a + b
 
     def subtract(self, a, b):
-        return b - a
+        return a - b
 
     def multiply(self, a, b):
         result = 0
-        for i in range(b+1):
+        if(a == 0 or b == 0): return 0
+        for i in range(b):
             result = self.add(result, a)
         return result
 
     def divide(self, a, b):
         result = 0
-        while a > b:
-            a = self.subtract(a, b)
-            result += 1
-        return result
+        if a == 0 :
+            return 0
+        if b == 0:
+            return "B can't be 0"
+        
+        if(a>0 and b>0):
+            while a >= b:
+                a = self.subtract(a, b)
+                result += 1
+            return result
+        
+        elif (a>0 and b<0):
+            while a >= b:
+                a = self.add(a, b)
+                result -= 1
+            return result+2
+        
+        elif (a<0 and b>0):
+            while a <= b:
+                a = self.add(a, b)
+                result -= 1
+            return result+2
+
+        elif (a<0 and b<0):
+            while a <= b:
+                a = self.subtract(a, b)
+                result += 1
+            return result
     
     def modulo(self, a, b):
-        while a <= b:
-            a = a-b
-        return a
+        if(b==0):
+            return "b can't be 0"
+        
+        if (a > 0 and b < 0 ):
+            while(1):
+                a += b
+                if(a <= 0):
+                    return a
+        
+        elif (a < 0 and b > 0):
+            while(1):
+                a += b
+                if(a >= 0):
+                    return a
+                
+        elif (a < 0 and b < 0):
+            while(a <= b):
+                a -= b
+            return a
+        else:
+            while a >= b:
+                a -= b
+            return a
+
 
 # Example usage:
 if __name__ == "__main__":
@@ -30,5 +76,5 @@ if __name__ == "__main__":
     print("Example: addition: ", calc.add(1, 2))
     print("Example: subtraction: ", calc.subtract(4, 2))
     print("Example: multiplication: ", calc.multiply(2, 3))
-    print("Example: division: ", calc.divide(10, 2))
-    print("Example: modulo: ", calc.modulo(10, 3))
+    print("Example: division: ", calc.divide(22, -5))
+    print("Example: modulo: ", calc.modulo(9, -4))
--- test_calculator.py
+++ test_calculator.py
@@ -8,8 +8,24 @@ class TestCalculator(unittest.TestCase):
 
     def test_add(self):
         self.assertEqual(self.calc.add(1, 2), 3)
-
+        self.assertEqual(self.calc.add(-1, -10), -11)
     # Add the following test methods to the TestCalculator class:
+    
+    def test_substract(self):
+        self.assertEqual(self.calc.subtract(-2, 8), -10)
+        self.assertEqual(self.calc.subtract(-1, -10), 9)
+
+    def test_multiply(self):
+        self.assertEqual(self.calc.multiply(6, 8), 48)
+        self.assertEqual(self.calc.multiply(-9, 7), -63)
+
+    def test_divide(self):
+        self.assertEqual(self.calc.divide(17,7), 2)
+        self.assertEqual(self.calc.divide(-111, -5), 22)
+
+    def test_modulo(self):
+        self.assertEqual(self.calc.modulo(213,-9), -3)
+        self.assertEqual(self.calc.modulo(-155, -7), -1)
 
 if __name__ == '__main__':
     unittest.main()
